8579b8d84d937d2a5373a376c2dcc706f226166a
Alastair McGilloway <alastairmcgilloway@hotmail.com>
2018-11-23 15:15:34 +0000

    Adds snake and ladders}{

---
+++ week_2/day_5/snakes_and_ladders/modifier.rb
@@ -0,0 +1,10 @@
+class Modifier
+
+attr_reader :start_position, :spaces_to_travel
+
+  def initialize(start_position, spaces_to_travel)
+    @start_position = start_position
+    @spaces_to_travel = spaces_to_travel
+  end
+
+end
