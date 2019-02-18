fd591dbdbcbe4cbe9d010a822cf8490530315793
Steven Meiklejohn <stevenmeiklejohn700@gmail.com>
2019-02-18 14:48:49 +0000

    Added Spring day 1 notes

---
+++ week_13/day_1/intro/pirateservice 4/src/test/java/com/codeclan/example/pirateservice/PirateserviceApplicationTests.java
@@ -0,0 +1,27 @@
+package com.codeclan.example.pirateservice;
+
+import com.codeclan.example.pirateservice.repositories.PirateRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PirateserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Autowired
+	PirateRepository pirateRepository;
+
+	@Test
+	public void createPirate(){
+		Pirate jack = new Pirate("Jack", "Sparrow", 32);
+		pirateRepository.save(jack);
+	}
+
+}
