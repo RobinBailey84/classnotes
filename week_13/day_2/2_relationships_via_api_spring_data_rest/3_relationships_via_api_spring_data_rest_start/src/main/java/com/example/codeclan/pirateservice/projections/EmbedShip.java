5de93fd0e0b1580abaa5dba4309d0a52f24a90a8
Alistair Kane <alistairkane92@hotmail.co.uk>
2019-02-19 19:36:19 +0000

    adds day 2

---
+++ week_13/day_2/2_relationships_via_api_spring_data_rest/3_relationships_via_api_spring_data_rest_start/src/main/java/com/example/codeclan/pirateservice/projections/EmbedShip.java
@@ -0,0 +1,17 @@
+package com.example.codeclan.pirateservice.projections;
+
+import com.example.codeclan.pirateservice.models.Pirate;
+import com.example.codeclan.pirateservice.models.Raid;
+import com.example.codeclan.pirateservice.models.Ship;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.util.List;
+
+@Projection(name="embedShip", types= Pirate.class)
+public interface EmbedShip {
+    String getFirstName();
+    String getLastName();
+    int getAge();
+    Ship getShip();
+    List<Raid> getRaids();
+}
